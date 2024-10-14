9a32b779c14743aa4197b09b28515d8318ca4cd9
ClaireZeng668 <claire.a.zeng@gmail.com>
2024-10-13 22:06:58 -0400

    some progress, still not sure whats going on tho

--- assignment3/cache.cpp
+++ assignment3/cache.cpp
@@ -1,6 +1,7 @@
 //cache related logic implementation
 
 #include "cache.h"
+#include <cstdint>
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
@@ -8,14 +9,31 @@
 #include <cstdlib>
 #include <cmath>
 
+
+
 Cache::Cache(int sets, int blocks, int blockSize, const std::string& writeAllocate, const std::string& writePolicy, const std::string& evictionPolicy)
     : sets(sets), blocks(blocks), blockSize(blockSize), writeAllocate(writeAllocate), writePolicy(writePolicy), evictionPolicy(evictionPolicy),
-      totalLoads(0), totalStores(0), loadHits(0), loadMisses(0), storeHits(0), storeMisses(0), totalCycles(0) {
-        
+      totalLoads(0), totalStores(0), loadHits(0), loadMisses(0), storeHits(0), storeMisses(0), totalCycles(0), time(0) {
+        std::vector<Set> set_vec;
       }
 
 void Cache::load(unsigned int address) {
     totalLoads++;
+     
+    if (sets > 1) {
+        //direct mapped
+        if (blocks == 1) {
+
+        } else { //set associative
+            int index = get_index_bits(address);
+            int tag = get_tag_bits(address);
+            
+        }
+    } else { //fully associative
+
+    }
+
+    
     //todo: implement functionality
 }
 
@@ -32,6 +50,31 @@ void Cache::printSummary() {
     std::cout << "Store hits: " << storeHits << std::endl;
     std::cout << "Store misses: " << storeMisses << std::endl;
     std::cout << "Total cycles: " << totalCycles << std::endl;
+}
+
+
+int Cache::get_num_offset_bits (unsigned int address, int label) {
+    int num_bits = 0;
+    int result = label;
+    while (result > 1) {
+        num_bits++;
+        result = result >> 1;
+    }
+    return num_bits;
+}
+
+int Cache::get_index_bits (unsigned int address) {
+    int num_offset_bits = get_num_offset_bits(address, blockSize);
+    unsigned int index_bits = address >> num_offset_bits;
+    index_bits = index_bits & (sets-1);
+    return index_bits;
+}
+
+int Cache::get_tag_bits (unsigned int address) {
+    int num_offset_bits = get_num_offset_bits(address, blockSize);
+    int num_index_bits = get_num_offset_bits(address, sets);
+    int total_offset = num_offset_bits + num_index_bits;
+    return address >> total_offset;
 }
 
 //helper functions to be added later
--- assignment3/cache.h
+++ assignment3/cache.h
@@ -1,6 +1,7 @@
 #ifndef CACHE_H
 #define CACHE_H
 
+#include <cstdint>
 #include <iostream>
 #include <vector>
 #include <cmath>
@@ -22,6 +23,7 @@ private:
     int storeHits;
     int storeMisses;
     int totalCycles;
+    int time;
 
     //config param for cache
     int sets;
@@ -32,6 +34,23 @@ private:
     std::string evictionPolicy;
 
     //add more later
+    struct Block {
+        uint32_t tag;
+        bool valid, dirty;
+        uint32_t load_ts, access_ts;
+
+        uint32_t get_tag();
+        bool get_validity();
+        uint32_t get_ts();
+    };
+
+    struct Set {
+        std::vector<Block> blocks;
+    };
+
+    int get_num_offset_bits (unsigned int address, int label);
+    int get_index_bits (unsigned int address);
+    int get_tag_bits (unsigned int address);
 };
 
 #endif
